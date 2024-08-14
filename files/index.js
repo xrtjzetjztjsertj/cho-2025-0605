829527d8a4f1f61065eba88f27f39ec101a0b321
Abbott Kasandra <102715619+phfdkgurhd@users.noreply.github.com>
2024-08-14 18:10:47 +1200

    Update index.js

--- files/index.js
+++ files/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const { exec, execSync } = require('child_process');
 const port = process.env.SERVER_PORT || process.env.PORT || 5000;        
-const UUID = process.env.UUID || '33675440-bfdc-459b-8a31-b68872c97579'; //若需要改UUID，需要在config.json里改为一致
+const UUID = process.env.UUID || '9f171b65-2a91-4d39-ab87-6a90c1f5a170'; //若需要改UUID，需要在config.json里改为一致
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'kcisya.dtjdhdtjft4ge4t.cf';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNTIwZmQxYWNjNjhjNDU3MzI0MTE1ZjAzYWY3YzA5YTIiLCJ0IjoiY2I4MzE2N2ItNGRiNS00YjExLWI1MmQtYjk1YWQ1YjU5YWY0IiwicyI6IllqUXhORFZoTW1ZdE5UQmlPUzAwT1RoaUxXRmlaREl0TkdJMk56QmpZVGs1TVdVMCJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'gcysua.nytvrexefegf.tk';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNTIwZmQxYWNjNjhjNDU3MzI0MTE1ZjAzYWY3YzA5YTIiLCJ0IjoiY2U2OWVkYTktMDM3Ni00YjU2LThlMDItNTliNjFjZTk2MjBjIiwicyI6Ik5UWTBaVGMxTlRndFl6Z3dNeTAwTjJWbExUbGlPVFV0WkdWbU9EWTVOR1V6T0dVNCJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
